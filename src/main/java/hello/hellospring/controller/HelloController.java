2444d57e16a82761c55649e2261841e5814b8c6c
khissup <khissup@naver.com>
2022-04-17 23:41:27 +0900

    2022-04-17 mvc commit

--- src/main/java/hello/hellospring/controller/HelloController.java
+++ src/main/java/hello/hellospring/controller/HelloController.java
@@ -3,6 +3,7 @@ package hello.hellospring.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class HelloController {
@@ -12,4 +13,19 @@ public class HelloController {
         model.addAttribute("data", "hello!!");
         return "hello";
     }
+
+    @GetMapping("hello-mvc")
+    public String helloMvc(@RequestParam("name") String name, Model model) {
+        model.addAttribute("name", name); // 앞에 "name"이 key, 뒤에 name이 실질적인 value
+        return "hello-template";
+    }
+    /*
+    RequestParam에서 아무것도 안해놓으면, required값은 기본으로 'true'로 잡혀있음. 매개변수 안넣고도
+    돌리고 싶은경우에는 아래처럼 required값을 false로 따로 바꿔줘야함. (위에같은경우 true로 되있는거랑 같음)
+    @GetMapping("hello-mvc")
+    public String helloMvc(@RequestParam(value = "name", required = false) String name, Model model) {
+        model.addAttribute("name", name); // 앞에 "name"이 key, 뒤에 name이 실질적인 value
+        return "hello-template";
+    }
+     */
 }
