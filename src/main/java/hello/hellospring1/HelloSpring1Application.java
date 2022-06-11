9f6c53e77c2364413e1142588ff82e66c0e3ce9f
khissup <khissup@naver.com>
2022-06-11 23:28:01 +0900

    spring-basic

--- src/main/java/hello/hellospring1/HelloSpring1Application.java
+++ src/main/java/hello/hellospring1/HelloSpring1Application.java
@@ -8,7 +8,7 @@ public class HelloSpring1Application {
 
 	public static void main(String[] args) {
 		SpringApplication.run(HelloSpring1Application.class, args);
-		// java version 변경
+		// java version 변경.
 	}
 
 }
